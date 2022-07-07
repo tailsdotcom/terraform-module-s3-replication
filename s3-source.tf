84174f082b98fd25e4d58781542c3a32990b94d4
sam-tails <sam@tails.com>
2022-07-07 12:02:22 +0100

    Update s3-source.tf

--- s3-source.tf
+++ s3-source.tf
@@ -9,7 +9,7 @@ data "aws_iam_policy_document" "source_replication_role" {
 
     principals {
       type        = "Service"
-      identifiers = ["s3.amazonaws.com"]
+      identifiers = ["s3.amazonaws.com", "batchoperations.s3.amazonaws.com"]
     }
   }
 }
